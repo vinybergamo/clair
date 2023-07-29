7c0d836a09dc2c8ec528860465a02f5eb3e55449
vinybergamo <vinybergamo@gmail.com>
2023-07-29 20:25:44 -0300

    feat: create apps command

---
+++ plugins/apps/src/commands/commands.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/vinybergamo/cloud/plugins/common"
+)
+
+const (
+	helpHeader = `Usage: cloud apps[:COMMAND]
+
+Manage apps
+
+Additional commands:`
+
+	helpContent = `
+    apps:clone <old-app> <new-app>, Clones an app
+    apps:create <app>, Create a new app
+    apps:destroy <app>, Permanently destroy an app
+    apps:exists <app>, Checks if an app exists
+    apps:list, List your apps
+    apps:lock <app>, Locks an app for deployment
+    apps:locked <app>, Checks if an app is locked for deployment
+    apps:rename <old-app> <new-app>, Rename an app
+    apps:report [<app>] [<flag>], Display report about an app
+    apps:unlock <app>, Unlocks an app for deployment
+`
+)
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+
+	cmd := flag.Arg(0)
+	switch cmd {
+	case "apps", "apps:help":
+		usage()
+	case "help":
+		command := common.NewShellCmd(fmt.Sprintf("ps -o command= %d", os.Getppid()))
+		command.ShowOutput = false
+		output, err := command.Output()
+
+		if err == nil && strings.Contains(string(output), "--all") {
+			fmt.Println(helpContent)
+		} else {
+			fmt.Print("\n    apps, Manage apps\n")
+		}
+	default:
+		cloudNotImplementExitCode, err := strconv.Atoi(os.Getenv("CLOUD_NOT_IMPLEMENTED_EXIT"))
+		if err != nil {
+			fmt.Println("failed to retrieve CLOUD_NOT_IMPLEMENTED_EXIT environment variable")
+			cloudNotImplementExitCode = 10
+		}
+		os.Exit(cloudNotImplementExitCode)
+	}
+}
+
+func usage() {
+	common.CommandUsage(helpHeader, helpContent)
+}
