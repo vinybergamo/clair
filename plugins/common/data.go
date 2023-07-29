def7fcc16b75aeaefedfebfef45f2a3c2f126014
vinybergamo <vinybergamo@gmail.com>
2023-07-29 17:14:22 -0300

    feat: create data

---
+++ plugins/common/data.go
@@ -0,0 +1,88 @@
+package common
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/otiai10/copy"
+)
+
+func CreateAppDataDirectory(pluginName, appName string) error {
+	directory := GetAppDataDirectory(pluginName, appName)
+	if err := os.MkdirAll(directory, 0755); err != nil {
+		return err
+	}
+
+	if err := SetPermissions(directory, 0755); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func CreateDataDirectory(pluginName string) error {
+	directory := GetDataDirectory(pluginName)
+	if err := os.MkdirAll(directory, 0755); err != nil {
+		return err
+	}
+
+	if err := SetPermissions(directory, 0755); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetAppDataDirectory(pluginName string, appName string) string {
+	return filepath.Join(GetDataDirectory(pluginName), appName)
+}
+
+func GetDataDirectory(pluginName string) string {
+	return filepath.Join(MustGetEnv("CLOUD_LIB_ROOT"), "data", pluginName)
+}
+
+func MigrateAppDataDirectory(pluginName string, oldAppName string, newAppName string) error {
+	if err := CloneAppData(pluginName, oldAppName, newAppName); err != nil {
+		return err
+	}
+
+	return RemoveAppDataDirectory(pluginName, oldAppName)
+}
+
+func RemoveAppDataDirectory(pluginName, appName string) error {
+	return os.RemoveAll(GetAppDataDirectory(pluginName, appName))
+}
+
+func CloneAppData(pluginName string, oldAppName string, newAppName string) error {
+	oldDataDir := GetAppDataDirectory(pluginName, oldAppName)
+	if !DirectoryExists(oldDataDir) {
+		return CreateAppDataDirectory(pluginName, newAppName)
+	}
+
+	newDataDir := GetAppDataDirectory(pluginName, newAppName)
+	if err := copy.Copy(oldDataDir, newDataDir); err != nil {
+		return fmt.Errorf("Unable to clone app data to new location: %v", err.Error())
+	}
+
+	return nil
+}
+
+func SetupAppData(pluginName string) error {
+	if err := CreateDataDirectory(pluginName); err != nil {
+		return err
+	}
+
+	apps, err := UnfilteredCloudApps()
+	if err != nil {
+		return nil
+	}
+
+	for _, appName := range apps {
+		if err := CreateAppDataDirectory(pluginName, appName); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
--- plugins/common/go.mod
+++ plugins/common/go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/codeskyblue/go-sh v0.0.0-20200712050446-30169cf553fe
 	github.com/onsi/gomega v1.27.10
+	github.com/otiai10/copy v1.12.0
 	github.com/ryanuber/columnize v2.1.2+incompatible
 	golang.org/x/sync v0.3.0
 )
@@ -13,6 +14,7 @@ require (
 	github.com/codegangsta/inject v0.0.0-20150114235600-33e0aa1cb7c0 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
