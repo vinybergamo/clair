2bdbdb05776c96f82249cc3d489b858d74477f01
vinybergamo <vinybergamo@gmail.com>
2023-07-29 17:20:28 -0300

    feat: create triggers/main package

---
+++ plugins/common/src/triggers/trigger.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/vinybergamo/cloud/plugins/common"
+)
+
+func main() {
+	parts := strings.Split(os.Args[0], "/")
+	trigger := parts[len(parts)-1]
+	flag.Parse()
+
+	var err error
+	switch trigger {
+	case "app-list":
+		filtered := flag.Arg(0)
+		if filtered == "" {
+			filtered = "true"
+		}
+		filter := common.ToBool(filtered)
+		err = common.TriggerAppList(filter)
+	case "core-post-deploy":
+		appName := flag.Arg(0)
+		err = common.TriggerCorePostDeploy(appName)
+	case "install":
+		err = common.TriggerInstall()
+	case "post-app-clone-setup":
+		oldAppName := flag.Arg(0)
+		newAppName := flag.Arg(1)
+		err = common.TriggerPostAppCloneSetup(oldAppName, newAppName)
+	case "post-app-rename-setup":
+		oldAppName := flag.Arg(0)
+		newAppName := flag.Arg(1)
+		err = common.TriggerPostAppRenameSetup(oldAppName, newAppName)
+	case "post-delete":
+		appName := flag.Arg(0)
+		err = common.TriggerPostDelete(appName)
+	default:
+		err = fmt.Errorf("Invalid plugin trigger call: %s", trigger)
+	}
+
+	if err != nil {
+		common.LogFailWithError(err)
+	}
+}
